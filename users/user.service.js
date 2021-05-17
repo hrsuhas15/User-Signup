2e72b8fcad5e395bef5003e2765970aed84cbdda
Suhas H R <40423884+hrsuhas15@users.noreply.github.com>
2021-05-17 10:01:29 +0530

    Add files via upload

---
+++ users/user.service.js
@@ -0,0 +1,35 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const User = db.User;
+
+module.exports = {
+    getAll,
+    create
+};
+
+async function getAll() {
+    return await User.find();
+}
+
+async function create(userParam) {
+    // validate
+    if (await User.findOne({ username: userParam.username })) {
+        throw 'Username ' + userParam.username + ' is already taken';
+    }
+
+    if (await User.findOne({ email: userParam.email })) {
+        throw 'Email ' + userParam.email + ' is already taken';
+    }
+
+    const user = new User(userParam);
+
+    // hash password
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // save user
+    await user.save();
+}
---
+++ users/users.controller.js
@@ -0,0 +1,23 @@
+﻿const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+
+
+// routes
+router.post('/signup', signup);
+router.get('/', getAll);
+
+module.exports = router;
+
+
+function signup(req, res, next) {
+    userService.create(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(err => next(err));
+}
